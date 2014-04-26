250b5d3395277dbcd8b767df872b94e0958a5a1a
Jonathan Kummerfeld <jkk@berkeley.edu>
2014-04-26 14:18:02 -0700

    * Fixing author errors

--- src/page_gen.py
+++ src/page_gen.py
@@ -4,97 +4,97 @@ import glob, os
 import pub_gen
 
 def get_tab_depth(line):
-	depth = 0
-	for letter in line:
-		if letter == '\t':
-			depth += 1
-		else:
-			break
-	return depth
+  depth = 0
+  for letter in line:
+    if letter == '\t':
+      depth += 1
+    else:
+      break
+  return depth
 
 def gen_menu(depth, page_names, url_base):
-	tab = '\t' * depth
-	ans = ''
-	for name in page_names[::-1]:
-		label = name
-		if '|' in name:
-			label = name.split('|')[1]
-			name = name.split('|')[0]
-		info = {'location': name + '.html', 'name': label, 'url_base': url_base}
-		ans += tab + '<ul class="menu_links">\n'
-		ans += tab + '\t' + ('<li><a href="%(url_base)s%(location)s">%(name)s</a></li>\n' % info)
-		ans += tab + '</ul>\n'
-	return ans
+  tab = '\t' * depth
+  ans = ''
+  for name in page_names[::-1]:
+    label = name
+    if '|' in name:
+      label = name.split('|')[1]
+      name = name.split('|')[0]
+    info = {'location': name + '.html', 'name': label, 'url_base': url_base}
+    ans += tab + '<ul class="menu_links">\n'
+    ans += tab + '\t' + ('<li><a href="%(url_base)s%(location)s">%(name)s</a></li>\n' % info)
+    ans += tab + '</ul>\n'
+  return ans
 
 def gen_software(source, indent, config, page_name, url_base, target_dir, pub_info):
-	software = []
-	cur = {}
-	end = ''
-	in_end = False
-	for line in source:
-		line = line.strip()
-		if len(line) == 0:
-			continue
-		elif line == 'RAW:':
-			in_end = True
-		elif in_end:
-			end += indent + line + '\n'
-		elif line == '{':
-			cur = {}
-		elif line == '}':
-			software.append(cur)
-		else:
-			field = line.split(' = ')[0]
-			value = ' = '.join(line.split(' = ')[1:])
-			cur[field] = value
-	
-	page = ''
-	for system in software:
-		page += indent + '<p>\n'
-		page += indent + '\t<a href="%(url)s">%(name)s</a>\n' % system
-		page += indent + '<br />\n'
-		page += indent + '\t%(description)s\n' % system
-		page += indent + '<br />\n'
-		cite = pub_gen.replace_pub_ref(system['paper'], config, page_name, url_base, target_dir, pub_info)
-		for part in cite:
-			page += indent + part + '\n'
-		page += indent + '</p>\n'
-		page += indent + '<br />\n'
-	page += end
-	return page
+  software = []
+  cur = {}
+  end = ''
+  in_end = False
+  for line in source:
+    line = line.strip()
+    if len(line) == 0:
+      continue
+    elif line == 'RAW:':
+      in_end = True
+    elif in_end:
+      end += indent + line + '\n'
+    elif line == '{':
+      cur = {}
+    elif line == '}':
+      software.append(cur)
+    else:
+      field = line.split(' = ')[0]
+      value = ' = '.join(line.split(' = ')[1:])
+      cur[field] = value
+  
+  page = ''
+  for system in software:
+    page += indent + '<p>\n'
+    page += indent + '\t<a href="%(url)s">%(name)s</a>\n' % system
+    page += indent + '<br />\n'
+    page += indent + '\t%(description)s\n' % system
+    page += indent + '<br />\n'
+    cite = pub_gen.replace_pub_ref(system['paper'], config, page_name, url_base, target_dir, pub_info)
+    for part in cite:
+      page += indent + part + '\n'
+    page += indent + '</p>\n'
+    page += indent + '<br />\n'
+  page += end
+  return page
 
 def gen_page(page_template, page_content, config, page_name, url_base, target_dir, pub_info):
-	page = ''
-	for line in page_template:
-		if line != '' and line[-1] == '\n':
-			line = line[:-1]
-		# nothing to insert
-		if '%' not in line:
-			page += line + '\n'
-		# page name
-		if '%(page_name)s' in line:
-			page += (line % {'page_name': page_name}) + '\n'
-		if '%(menu)s' in line:
-			depth = get_tab_depth(line)
-			page += gen_menu(depth, config['page_order'], url_base)
-		if '%(content)s' in line:
-			depth = get_tab_depth(line)
-			tab = '\t' * depth
-			if 'software' in page_name:
-				page += gen_software(page_content, tab, config, page_name, url_base, target_dir, pub_info)
-			else:
-				for line in page_content:
-					if line != '' and line[-1] =='\n':
-						line = line[:-1]
-					if '%(pub' in line:
-						replacement = pub_gen.replace_pub_ref(line, config, page_name, url_base, target_dir, pub_info)
-						for part in replacement:
-							page += tab + part + '\n'
-					else:
-						page += tab + line + '\n'
+  page = ''
+  for line in page_template:
+    if line != '' and line[-1] == '\n':
+      line = line[:-1]
+    # nothing to insert
+    if '%' not in line:
+      page += line + '\n'
+    # page name
+    if '%(page_name)s' in line:
+      page += (line % {'page_name': page_name}) + '\n'
+    if '%(menu)s' in line:
+      depth = get_tab_depth(line)
+      page += gen_menu(depth, config['page_order'], url_base)
+    if '%(content)s' in line:
+      depth = get_tab_depth(line)
+      tab = '\t' * depth
+      if 'software' in page_name:
+        page += gen_software(page_content, tab, config, page_name, url_base, target_dir, pub_info)
+      else:
+        for line in page_content:
+          if line != '' and line[-1] =='\n':
+            line = line[:-1]
+          if '%(pub' in line:
+            replacement = pub_gen.replace_pub_ref(line, config, page_name, url_base, target_dir, pub_info)
+            for part in replacement:
+              page += tab + part + '\n'
+          else:
+            page += tab + line + '\n'
 
-	# Print the page to the target directory
-	out = open(config['target_dir'] + '/' + page_name, 'w')
-	print >> out, page
-	out.close()
+  # Print the page to the target directory
+  out = open(config['target_dir'] + '/' + page_name, 'w')
+  print >> out, page
+  out.close()
 
--- src/site_gen.py
+++ src/site_gen.py
@@ -5,85 +5,85 @@ from page_gen import *
 from pub_gen import *
 
 if __name__ == '__main__':
-	# Config
-	config = {
-		'target_dir': './',
-		'data_dir': '/home/jonathan/Dropbox/jkk_name_data/',
-		'url_base': '',
-		'page_template': 'templates/base.html'
-	}
-	if len(sys.argv) == 2:
-		for line in open(sys.argv[1]):
-			line = line.strip()
-			if line == '' or line[0] == '#':
-				continue
-			parts = line.split('=')
-			key = parts[0].strip()
-			value = parts[1].strip()
-			if value.strip() != '':
-				if ':' in value:
-					config[key] = {}
-					for pair in value.split(' '):
-						pair = pair.split(':')
-						config[key][pair[0]] = pair[1]
-				elif ' ' in value:
-					config[key] = []
-					for part in value.split(' '):
-						config[key].append(part)
-				else:
-					config[key] = value
+  # Config
+  config = {
+    'target_dir': './',
+    'data_dir': '/home/jonathan/Dropbox/jkk_name_data/',
+    'url_base': '',
+    'page_template': 'templates/base.html'
+  }
+  if len(sys.argv) == 2:
+    for line in open(sys.argv[1]):
+      line = line.strip()
+      if line == '' or line[0] == '#':
+        continue
+      parts = line.split('=')
+      key = parts[0].strip()
+      value = parts[1].strip()
+      if value.strip() != '':
+        if ':' in value:
+          config[key] = {}
+          for pair in value.split(' '):
+            pair = pair.split(':')
+            config[key][pair[0]] = pair[1]
+        elif ' ' in value:
+          config[key] = []
+          for part in value.split(' '):
+            config[key].append(part)
+        else:
+          config[key] = value
 
-	target_dir = config['target_dir']
-	data_dir = config['data_dir']
-	url_base = config['url_base']
+  target_dir = config['target_dir']
+  data_dir = config['data_dir']
+  url_base = config['url_base']
 
-	pages = [name for name in glob.glob(data_dir + '/pages/*')]
-	if 'page_order' not in config:
-		page_order = []
-		for page in pages:
-			if '.html' in page:
-				page = page[:-5]
-			page_order.append(page)
-		config['page_order'] = page_order
-	
-	print "Running with:"
-	for key in config:
-		print key + '\t' + config[key].__str__()
-	print
-	user_response = raw_input("Run? (check directories)")
-	if 'n' in user_response.lower():
-		sys.exit(0)
+  pages = [name for name in glob.glob(data_dir + '/pages/*')]
+  if 'page_order' not in config:
+    page_order = []
+    for page in pages:
+      if '.html' in page:
+        page = page[:-5]
+      page_order.append(page)
+    config['page_order'] = page_order
+  
+  print "Running with:"
+  for key in config:
+    print key + '\t' + config[key].__str__()
+  print
+  user_response = raw_input("Run? (check directories)")
+  if 'n' in user_response.lower():
+    sys.exit(0)
 
-	# Clean up the target space
-	if len(glob.glob(target_dir)) != 0:
-		os.system('rm -r %s/docs' % target_dir)
-		os.system('rm -r %s/images' % target_dir)
-		os.system('rm -r %s/pubs' % target_dir)
-	os.system('mkdir -p %s/docs' % target_dir)
-	os.system('mkdir %s/images' % target_dir)
-	os.system('mkdir %s/pubs' % target_dir)
-	os.system('cp %s/templates/* %s/.' % (data_dir, target_dir))
-	os.system('cp -R %s/images/* %s/images/.' % (data_dir, target_dir))
-	os.system('cp -R %s/docs/* %s/docs/.' % (data_dir, target_dir))
-	os.system('cp -R %s/pubs/publications.bib %s/pubs/jonathan_k_kummerfeld_publications.bib' % (data_dir, target_dir))
-	os.system('rm -rf %s/pubs/citations*' % (target_dir))
-	os.system('cp -R %s/pubs/*/*_talk* %s/pubs/.' % (data_dir, target_dir))
-	os.system('cp -R %s/pubs/*/*_poster* %s/pubs/.' % (data_dir, target_dir))
+  # Clean up the target space
+  if len(glob.glob(target_dir)) != 0:
+    os.system('rm -r %s/docs' % target_dir)
+    os.system('rm -r %s/images' % target_dir)
+    os.system('rm -r %s/pubs' % target_dir)
+  os.system('mkdir -p %s/docs' % target_dir)
+  os.system('mkdir %s/images' % target_dir)
+  os.system('mkdir %s/pubs' % target_dir)
+  os.system('cp %s/templates/* %s/.' % (data_dir, target_dir))
+  os.system('cp -R %s/images/* %s/images/.' % (data_dir, target_dir))
+  os.system('cp -R %s/docs/* %s/docs/.' % (data_dir, target_dir))
+  os.system('cp -R %s/pubs/publications.bib %s/pubs/jonathan_k_kummerfeld_publications.bib' % (data_dir, target_dir))
+  os.system('rm -rf %s/pubs/citations*' % (target_dir))
+  os.system('cp -R %s/pubs/*/*_talk* %s/pubs/.' % (data_dir, target_dir))
+  os.system('cp -R %s/pubs/*/*_poster* %s/pubs/.' % (data_dir, target_dir))
 
-	# Construct publication info
-	pub_info = pub_gen.get_pub_info(data_dir, target_dir)
-	for pub in pub_info:
-		if 'url' not in pub_info[pub]:
-			if 'school' in pub_info[pub]:
-				os.system('cp -R %s/pubs/%s/*thesis* %s/pubs/.' % (data_dir, pub, target_dir))
-			else:
-				os.system('cp -R %s/pubs/%s/*paper* %s/pubs/.' % (data_dir, pub, target_dir))
+  # Construct publication info
+  pub_info = pub_gen.get_pub_info(data_dir, target_dir)
+  for pub in pub_info:
+    if 'url' not in pub_info[pub]:
+      if 'school' in pub_info[pub]:
+        os.system('cp -R %s/pubs/%s/*thesis* %s/pubs/.' % (data_dir, pub, target_dir))
+      else:
+        os.system('cp -R %s/pubs/%s/*paper* %s/pubs/.' % (data_dir, pub, target_dir))
 
-	# Construct pages
-	page_template = open(data_dir + '/' + config['page_template']).readlines()
-	for page_location in pages:
-		page_content = open(page_location).readlines()
-		page_name = page_location.split('/')[-1]
-		print 'Generating', page_name
-		gen_page(page_template, page_content, config, page_name, url_base, target_dir, pub_info)
+  # Construct pages
+  page_template = open(data_dir + '/' + config['page_template']).readlines()
+  for page_location in pages:
+    page_content = open(page_location).readlines()
+    page_name = page_location.split('/')[-1]
+    print 'Generating', page_name
+    gen_page(page_template, page_content, config, page_name, url_base, target_dir, pub_info)
 
